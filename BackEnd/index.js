6ad4a330f25e8b16ba10af11b4a961263bb4d0e6
aneliaspe <antoniosnake6@gmail.com>
2026-07-27 10:15:55 -0700

    descativacion de links

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -86,6 +86,7 @@ const Empleados = mongoose.model(
 );
 
 // OBTENER TODOS LOS EMPLEADOS
+/* 
 app.get("/empleados", async (req, res) => {
     try {
 
@@ -972,7 +973,7 @@ app.delete("/productos/:id", async (req, res) => {
 
 
 
-
+*/
     app.get("/", (req, res) => {
     res.send("API del Proyecto NoSQL");
     });
