291dbcd4194baf8a920ed2da7956fae6c8023a41
aneliaspe <antoniosnake6@gmail.com>
2026-07-28 09:17:01 -0700

    cambios

--- Frontend/config.js
+++ Frontend/config.js
@@ -3,4 +3,4 @@
 // Configuración global de la API
 // ==========================================================
 //capa de  comunicacion de HTTP 
-export const API_URL = "https://tu-api-desplegada.vercel.app"; 
+export const API_URL = "https://apimitiendita-cs1mjygi8-antonioelias.vercel.app/"; 
